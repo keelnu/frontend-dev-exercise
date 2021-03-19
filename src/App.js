3be541b33b34deb9e4e49f8ea045fc0ac846d449
Kristiina Eelnurme <kristiina.eelnurme@gmail.com>
2021-03-19 15:20:55 -0400

    Modified CSS modules

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import './styles/index.css';
 import styles from './styles/App.module.css';
-import formStyles from './styles/FormContainer.module.css';
 import FormHeader from './components/FormHeader';
-import Terms from './components/Terms';
-import Button from './components/Button';
+import FormContents from './components/FormContents';
 
 function App() {
   return (
     <div className={styles['form-container']}>
       <FormHeader />
-      <form className={formStyles['']}>
-        <fieldset className={formStyles['form-group']}>
-          <input
-            name="business"
-            className={formStyles['form-input']}
-            placeholder='What is the name of your business?'
-          />
-          <p className={formStyles['required-text']}>
-            Business Name Required
-          </p>
-        </fieldset>
-        <Terms />
-        <Button />
-      </form>
-      {/* <Button /> */}
+      <FormContents />
     </div>
   );
 }
---
+++ src/components/FormContents.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import styles from '../styles/FormContents.module.css';
+import Terms from './Terms';
+import Button from './Button';
+
+const FormContents = () => {
+
+  // logic for form to go here
+
+  return (
+    <div>
+      <form>
+        <fieldset className={styles['form-fields']}>
+          <input
+            name="name"
+            className={styles['form-input']}
+            placeholder='What is the name of your business?'
+          />
+          <p className={styles['required-text']}>
+            Business Name Required
+          </p>
+          <input
+            name="website"
+            className={styles['form-input']}
+            placeholder='Business website'
+          />
+          <p className={styles['required-text']}>
+            Business Website Required
+          </p>
+          <select
+            name="ticketing-system"
+            className={styles['form-input']} id="system-name">
+              <option value="select">Select your ticketing system</option>
+              <option value="zendesk">Zendesk</option>
+              <option value="intercom">Intercom</option>
+              <option value="gorgias">Gorgias</option>
+            </select>
+            <p class="required-text">Ticketing System Required</p>
+          <input
+            name="email"
+            className={styles['form-input']}
+            placeholder='Your email'
+          />
+          <p className={styles['required-text']}>
+            Email Required
+          </p>
+          <input
+            name="password"
+            className={styles['form-input']}
+            placeholder='Password'
+          />
+          <p className={styles['required-text']}>
+            Password Required
+          </p>
+        </fieldset>
+        <Terms />
+        <Button />
+      </form>
+    </div>
+  )
+}
+
+export default FormContents;
